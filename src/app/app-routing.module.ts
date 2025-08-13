5590de4b3666294335931a033b312b8ce22bc089
nithyanand1997 <nithyanand1997@gmail.com>
2025-08-14 01:04:05 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { PaperPublishingComponent } from './components/paper-publishing/paper-publishing.component';
 
 
 const routes: Routes = [
-  { path: '/', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'about-us', component: AboutUsComponent },
+  { path: 'paper-publishing', component: PaperPublishingComponent },
 ];
 
 @NgModule({
